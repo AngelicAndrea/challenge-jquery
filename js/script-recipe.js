b333d5994bfd2a2a0e410f231a2d15890a0c1300
AngelicAndrea <angelica.andrea05@gmail.com>
2018-01-08 17:26:58 -0300

    Etapa 10 lista

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -4,5 +4,15 @@ $(document).ready( function(){
    $(".js-back").show();
    $(".js-menu").hide();
     
+ //Etapa 10 
+
+ $(".js-back").on("click",function(atras){
+    if (atras){
+        var url = "index.html"
+        $(location).attr("href",url);
+    }
+
+});
+
 });
     
