a1efd0a06626ed0dfa6b40598c6e482af2efa325
AngelicAndrea <angelica.andrea05@gmail.com>
2018-01-08 15:11:52 -0300

    Etapa 1 listo

--- js/script-index.js
+++ js/script-index.js
@@ -3,6 +3,11 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
+
+
+	/*función ocultar flecha */
+
+	$(".js-back").hide();
 });
 
 
